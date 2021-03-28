ad42626fb40c2ec0cc780682a4f16122eec8acd0
anamariabuiciuc <anamariab1612@gmail.com>
2021-03-28 14:47:21 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+//Flowershop Online
+//in progress
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/company/flowerShopAddress.java
@@ -0,0 +1,48 @@
+package com.company;
+
+public class flowerShopAddress {
+
+    private String county;
+    private String town;
+    private String street;
+    private String number; //It is a String because the number can have a form like "12B".
+    private int postalCode;
+
+    public flowerShopAddress(String county, String town, String street, String number, int postalCode)
+    {
+        this.county = county;
+        this.town = town;
+        this.street = street;
+        this.number = number;
+        this.postalCode = postalCode;
+    }
+
+    public void setCountyFlowerShopAddress(String county)
+    {
+        this.county = county;
+    }
+    public void setTownFlowerShopAddress(String town)
+    {
+        this.town = town;
+    }
+
+    public void setStreetFlowerShopAddress(String street)
+    {
+         this.street = street;
+    }
+
+    public void setNumberFlowerShopAddress(String number)
+    {
+        this.number = number;
+    }
+
+    public void setPostalCodeFlowerShopAddress(int PostalCode)
+    {
+        this.postalCode = postalCode;
+    }
+
+    public String getTownFlowerShopAddress(String town)
+    {
+        return town;
+    }
+}
